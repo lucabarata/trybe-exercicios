5b06e946b1b60e28b99945c9a6b3eb125530635f
luca <luca.baruselli@gmail.com>
2022-05-13 11:00:29 -0300

    cria exercicios

---
+++ exercicios/fundamentos/bloco-04-introducao-a-javascrip-e-logica-de-programacao/dia-1-primeiros-passos/exercicios/exercicio03.js
@@ -0,0 +1,22 @@
+const a = 15;
+const b = 15;
+const c = 15;
+
+if (a > b && a > c) {
+    console.log("Maior = a")
+}
+else if (b > a && b > c) {
+    console.log("Maior = b")
+}
+else if (c > a && c > b) {
+    console.log("Maior = c")
+}
+else if (a == b) {
+    console.log("A=B")
+}
+else if (b==c) {
+    console.log("B=C")
+}
+else if (a==c) {
+    console.log("A=C")
+}
