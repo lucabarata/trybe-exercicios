2367de6cfbe4d0aadac351350fa7741a16900398
luca <luca.baruselli@gmail.com>
2022-05-17 10:30:23 -0300

    cria exercicios dia 3

---
+++ exercicios/fundamentos/bloco-04-introducao-a-javascrip-e-logica-de-programacao/dia-3-js-logica-de-programacao-e-algoritmos/aula/exemplo1.js
@@ -0,0 +1,10 @@
+soma = 0
+let fruits = [3, 4, 10, 1, 12];
+for(n of fruits) {
+    soma += n
+}
+if(soma>15) {
+    console.log(soma)
+} else if(soma<=15) {
+    console.log("valor menor que 16")
+}
